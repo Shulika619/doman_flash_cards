60df1746937c76338c2fb80c2a532421a7ef0aa4
Shulika Sergey <shulika619@gmail.com>
2022-04-23 19:21:50 +0300

    start create main page

---
+++ lib/const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const String kTitle = 'Карточки Домана';
+
+const kGradientBoxDecoration = BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [Colors.purple, Colors.orange]));
